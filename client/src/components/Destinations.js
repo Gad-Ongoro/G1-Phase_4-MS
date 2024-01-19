92fe8eadbbc8a8ae5c6587554dbff0cf55215963
Gad-Ongoro <gadongoro1@gmail.com>
2024-01-19 12:19:53 +0300

    Added Components

---
+++ client/src/components/Destinations.js
@@ -0,0 +1,42 @@
+import React from 'react'
+
+export default function Destinations() {
+  	return (
+    	<div className='hot_deals container'>
+			<h3>Top Destinations</h3>
+			<div className='card_container d-flex gap-5'>
+				<div className='hot_deal_card mt-2'>
+					<img src='https://viutravel-cms-bucket.s3.eu-west-1.amazonaws.com/556/conversions/Aerial-thumbnail.webp' alt='NA'></img>
+					<p>Location</p>
+					<p>Rating</p>
+					<p>Price</p>
+					<button>Book Now</button>
+				</div>
+
+				<div className='hot_deal_card mt-2'>
+					<img src='https://viutravel-cms-bucket.s3.eu-west-1.amazonaws.com/556/conversions/Aerial-thumbnail.webp' alt='NA'></img>
+					<p>Location</p>
+					<p>Rating</p>
+					<p>Price</p>
+					<button>Book Now</button>
+				</div>
+
+				<div className='hot_deal_card mt-2'>
+					<img src='https://viutravel-cms-bucket.s3.eu-west-1.amazonaws.com/556/conversions/Aerial-thumbnail.webp' alt='NA'></img>
+					<p>Location</p>
+					<p>Rating</p>
+					<p>Price</p>
+					<button>Book Now</button>
+				</div>
+
+				<div className='hot_deal_card mt-2'>
+					<img src='https://viutravel-cms-bucket.s3.eu-west-1.amazonaws.com/556/conversions/Aerial-thumbnail.webp' alt='NA'></img>
+					<p>Location</p>
+					<p>Rating</p>
+					<p>Price</p>
+					<button>Book Now</button>
+				</div>
+			</div>
+    	</div>
+  	)
+}
