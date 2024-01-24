57666b289b99fe5eb049d35d0ad070b9e27cf5c1
Gad-Ongoro <gadongoro1@gmail.com>
2024-01-25 00:41:10 +0300

    Accommodations

--- client/src/components/Accommodations.js
+++ client/src/components/Accommodations.js
@@ -1,17 +1,58 @@
 import React from 'react';
 
-export default function Accommodations({accommodations}) {
-	console.log(accommodations)
+export default function Accommodations({accommodations, setAccommodations}) {
+	// let [new_hotel_name, setNewHotelName] = useState({})
 
 	let accommodation_cards = accommodations.map((accommodation) => {
 		return(
 			<div className='safari_card mt-2' key={accommodation.accommodation_id}>
-				<img src={accommodation.thumbnail} alt='NA'></img>
+				<img src={accommodation.thumbnail} className='accommodation_thumbnail' alt='NA'></img>
 				<p>{accommodation.name}</p>
 				<p>{accommodation.location}</p>
 				<p>From Kes: {accommodation.price}</p>
 				<p>Rating: {accommodation.rating}</p>
-				<button>Book Now</button>
+
+				<img
+				className='edit-btn'
+				src='https://cdn-icons-png.flaticon.com/128/10336/10336582.png'
+				alt='NA'
+				onClick={() => {
+					fetch(`http://127.0.0.1:5000/accommodations/${accommodation.accommodation_id}`, {
+						method: "PATCH",
+						headers: {
+							'Content-Type' : 'application/json'
+						},
+						body: JSON.stringify()
+
+					})
+					.then(response => response.json())
+				}}
+				>
+				</img>
+
+				<img 
+				className='delete-btn'
+				onClick = {()=>{
+					fetch(`http://127.0.0.1:5000/accommodations/${accommodation.accommodation_id}`,{
+						method: 'DELETE'
+					}
+					)
+					.then(res => res.json())
+					.then(() => {
+						let new_items = accommodations.filter((item) => {
+							return(
+								item.accommodation_id !== accommodation.accommodation_id
+							)
+						})
+						setAccommodations(new_items)
+					}
+					)
+				}}
+				src='https://cdn-icons-png.flaticon.com/128/6861/6861362.png' 
+				alt='NA'>
+				</img>
+
+				<button className='d-block'>Book Now</button>
 			</div>
 		);
 	}
