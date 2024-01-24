c7d8c37d6bc8ce6c9b9487598bb462e3c948d3fa
Gad-Ongoro <gadongoro1@gmail.com>
2024-01-24 08:24:10 +0300

    useState

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,20 @@
+import React, {useState} from 'react';
+import { Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
-import Footer from './components/Footer';
 import Main from './components/Main';
+import Footer from './components/Footer';
 import UserLog from './components/UserLog';
 import './App.css';
 
 function App() {
+	let [bookings] = useState(0);
+	
   	return (
     	<div className="App">
-		  	<Header></Header>
-			<UserLog></UserLog>
+			<Header bookings = {bookings}></Header>
+			<Routes>
+				<Route path='/account' exact element={<UserLog></UserLog>}></Route>
+			</Routes>
 			<Main></Main>
 			<Footer></Footer>
     	</div>
