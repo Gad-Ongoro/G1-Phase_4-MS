b2b9c34500f291c971ecf2e9d055b1cc80b9cba8
Gad-Ongoro <gadongoro1@gmail.com>
2024-01-26 10:57:07 +0300

    Logic to habdle userLog

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,7 +1,30 @@
-import React from 'react';
+import React, {useState, useContext} from 'react';
 import { NavLink } from 'react-router-dom';
+import { SafarisContext } from '../App';
 
-export default function Header() {
+export default function Header({bookings, handleLogOut}) {
+    let {searchTerm, setSearchTerm, signedIn, setSignedIn} = useContext(SafarisContext);
+    let [vacations, setVacations] = useState(true);
+    // let [stays, setStays] = useState(false)
+
+    function onSearchChange(e){
+        setSearchTerm(e.target.value)
+        console.log(searchTerm);
+    }
+    function handleSearchSubmit(e){
+        e.preventDefault();
+    }
+
+    function handleVacationsClick(){
+        setVacations(true)
+    }
+
+    function handleStaysClick(){
+        setVacations(false)
+        // setStays(true)
+    }
+
+
     return (
         <header className='header px-5 pt-2 pb-5 text-white'>
             <div className='header-nav-container-1 container-fluid d-flex justify-content-between'>
@@ -15,10 +38,14 @@ export default function Header() {
                     <img className='language_img' src='https://t-cf.bstatic.com/design-assets/assets/v3.109.0/images-flags/Gb@3x.png' alt='NA'></img>
                     <img className='support_img' src='https://cdn-icons-png.flaticon.com/128/10309/10309127.png' alt='NA'></img>
                     <p>
-                        <NavLink to='/bookings' className='text-white nav_links_2'>Manage Booking</NavLink>
+                        <NavLink to='/bookings' className='text-white nav_links_2'><div className='bookings d-inline'>Manage Bookings <p className='booking_count'>{bookings}</p></div></NavLink>
                     </p>
                     <p>
-                        <NavLink to='/account' className='text-white nav_links_2' exact>Sign in</NavLink>
+                        {signedIn ?
+                            <NavLink to='/accommodations' className='text-white nav_links_2' exact onClick={handleLogOut}>Logout</NavLink>
+                            :
+                            <NavLink to='/account' className='text-white nav_links_2' exact>Sign in</NavLink>
+                        }                        
                     </p>
                     <p>
                         <NavLink to='/account' className='text-white' exact>
@@ -28,22 +55,25 @@ export default function Header() {
                 </div>
             </div>
             <div className='header-nav-container-2 d-flex justify-content-around flex-wrap'>
+
                 <div className='service_div'>
-                    <NavLink to='/accommodations' className='nav_links_2'>Accommodations & Hotels</NavLink>
+                    <NavLink to='/vacations' onClick={handleVacationsClick} className='nav_links_2'>Vacations</NavLink>
                 </div>
 
                 <div className='service_div'>
-                    <NavLink to='/attractions' className='nav_links_2'>Attractions & Activities</NavLink>
+                    <NavLink to='/accommodations' onClick={handleStaysClick} className='nav_links_2'>Accommodations & Hotels</NavLink>
                 </div>
 
-                <div className='service_div'>                    
+                <div className='service_div'>
                     <a href='https://www.kenya-airways.com/en-us/' className='nav_links_2' target='_blank' rel="noreferrer">
                         Flights
-                    </a>                   
+                    </a>
                 </div>
 
                 <div className='service_div'>
-                    <NavLink to='/car_rentals' className='nav_links_2'>Car Rentals</NavLink>
+                    <a href='https://www.pigiame.co.ke/car-hire' className='nav_links_2' target='_blank' rel="noreferrer">
+                        Car Rentals
+                    </a>
                 </div>
 
                 <div className='service_div'>                    
@@ -53,18 +83,26 @@ export default function Header() {
                 </div>
             </div>
 
+            {vacations ?
             <div className='nav_links_info container mt-3 mb-0'>
                 <div>
-                    <h2> Car hire for any kind of trip </h2>
-                    <p> Great deals at great prices, from the biggest car hire companies </p>
+                    <h2> Attractions, activities, and experiences </h2>
+                    <p> Discover new attractions and experiences to match your interests and travel style </p>
                 </div>
             </div>
-            <div className='search_form py-1 px-3'>
-                <form className='d-flex gap-1'>
-                    <input type='text' className='' placeholder='Destination'></input>
-                    <input type='date'></input>
-                    <input type='date'></input>
-                    <input type='submit' value="Search"></input>                    
+            :
+            <div className='nav_links_info container mt-3 mb-0'>
+                <div>
+                    <h2> Find your next stay </h2>
+                    <p> Search deals on hotels, homes, and much more... </p>
+                </div>
+            </div>
+            }
+
+            <div className='search_form py-1 px-2'>
+                <form className='d-flex gap-1' onSubmit={handleSearchSubmit}>
+                    <input type='text' className='destination_input' name='destination' placeholder='Destination' onChange={onSearchChange} autoComplete='off'></input>
+                    <input type='submit' value="Search" className='search_btn text-white bg-primary'></input>                    
                 </form>
             </div>
         </header>
