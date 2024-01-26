910635afcad118c08ba387bae90c9266a69051f6
Gad-Ongoro <gadongoro1@gmail.com>
2024-01-26 10:56:22 +0300

    useContext

--- client/src/App.js
+++ client/src/App.js
@@ -3,15 +3,14 @@ import Header from './components/Header';
 import Main from './components/Main';
 import Footer from './components/Footer';
 import './App.css';
-import { useNavigate } from 'react-router-dom';
 export const SafarisContext = React.createContext();
 
 function App() {
-	let navigate = useNavigate()
 	let [signedIn, setSignedIn] = useState();
 	let [bookings] = useState(0);
 	let [vacations, setVacations] = useState([]);
 	let [accommodations, setAccommodations] = useState([])
+	let [searchTerm, setSearchTerm] = useState('Hello World')
 
 	useEffect(() => {
 		fetch("http://127.0.0.1:5000/vacations")
@@ -34,10 +33,16 @@ function App() {
 	[]
 	)
 
+	// HEADER
+	function handleLogOut(e){
+		document.cookie = `user_name = ; expires=Thu, 01 Jan 2000 00:00:00 UTC;`;
+		document.cookie = `customer_id = ; expires=Thu, 01 Jan 1970 00:00:00 UTC;`;
+	}
+
   	return (
     	<div className="App">
-			<SafarisContext.Provider value={{signedIn, setSignedIn}}>
-				<Header bookings = {bookings}></Header>
+			<SafarisContext.Provider value={{accommodations, setAccommodations, signedIn, setSignedIn, searchTerm, setSearchTerm}}>
+				<Header bookings = {bookings} handleLogOut = {handleLogOut}></Header>
 				<Main vacations = {vacations} accommodations = {accommodations} setAccommodations = {setAccommodations}></Main>
 				<Footer></Footer>
 			</SafarisContext.Provider>
