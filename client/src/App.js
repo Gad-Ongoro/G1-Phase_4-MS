c2a637c4c0e02c3ad865a61a959089aabb1c7564
Gad-Ongoro <gadongoro1@gmail.com>
2024-01-26 03:15:41 +0300

    Used useEffect to fetch

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,40 @@
-import React, {useState} from 'react';
-import { Routes, Route } from 'react-router-dom';
+import React, {useState, useEffect} from 'react';
 import Header from './components/Header';
 import Main from './components/Main';
 import Footer from './components/Footer';
-import UserLog from './components/UserLog';
 import './App.css';
 
 function App() {
+	document.cookie = "name=Gad Ongoro";
 	let [bookings] = useState(0);
-	
+	let [vacations, setVacations] = useState([]);
+	let [accommodations, setAccommodations] = useState([])
+
+	useEffect(() => {
+		fetch("http://127.0.0.1:5000/vacations")
+		.then(response => response.json())
+		.then(data => {
+			setVacations(data)
+		})
+	},
+	[]
+	)
+
+	useEffect(() => {
+		fetch("http://127.0.0.1:5000/accommodations")
+		.then(response => response.json())
+		.then(data => {
+			setAccommodations(data)
+			// console.log(data);
+		})
+	},
+	[]
+	)
+
   	return (
     	<div className="App">
 			<Header bookings = {bookings}></Header>
-			<Routes>
-				<Route path='/account' exact element={<UserLog></UserLog>}></Route>
-			</Routes>
-			<Main></Main>
+			<Main vacations = {vacations} accommodations = {accommodations} setAccommodations = {setAccommodations}></Main>
 			<Footer></Footer>
     	</div>
  	);
