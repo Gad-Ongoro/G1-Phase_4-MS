96c5a260b06599ad57b1ef5ba1b05bf11da44e1b
Gad-Ongoro <gadongoro1@gmail.com>
2024-01-29 13:35:51 +0300

    UseNavigate

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
-import React, {useState, useEffect, createContext} from 'react';
+import React, {useState, useEffect} from 'react';
+import { useNavigate } from 'react-router-dom';
 import Header from './components/Header';
 import Main from './components/Main';
 import Footer from './components/Footer';
@@ -6,8 +7,9 @@ import './App.css';
 export const SafarisContext = React.createContext();
 
 function App() {
+	let navigate = useNavigate()
 	let [signedIn, setSignedIn] = useState();
-	let [bookings] = useState(0);
+	let [bookings, setBookings] = useState(0);
 	let [vacations, setVacations] = useState([]);
 	let [accommodations, setAccommodations] = useState([])
 	let [searchTerm, setSearchTerm] = useState('Hello World')
@@ -37,11 +39,13 @@ function App() {
 	function handleLogOut(e){
 		document.cookie = `user_name = ; expires=Thu, 01 Jan 2000 00:00:00 UTC;`;
 		document.cookie = `customer_id = ; expires=Thu, 01 Jan 1970 00:00:00 UTC;`;
+		navigate('/signin')
+		setSignedIn(false)
 	}
 
   	return (
     	<div className="App">
-			<SafarisContext.Provider value={{accommodations, setAccommodations, signedIn, setSignedIn, searchTerm, setSearchTerm}}>
+			<SafarisContext.Provider value={{accommodations, setAccommodations, signedIn, setSignedIn, searchTerm, setSearchTerm, bookings, setBookings}}>
 				<Header bookings = {bookings} handleLogOut = {handleLogOut}></Header>
 				<Main vacations = {vacations} accommodations = {accommodations} setAccommodations = {setAccommodations}></Main>
 				<Footer></Footer>
