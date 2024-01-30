8ce02c1a133d8f4dc2d8fa88714c078275d2f659
Gad-Ongoro <gadongoro1@gmail.com>
2024-01-30 09:20:56 +0300

    Vacations

--- client/src/components/Vacations.js
+++ client/src/components/Vacations.js
@@ -1,41 +1,25 @@
 import React from 'react';
 
-export default function Vacations() {
+export default function Vacations({vacations}) {
+
+	let vacay_cards = vacations.map((vacay) => {
+		return(
+			<div className='safari_card mt-2' key={vacay.vacation_id}>
+				<img src={vacay.thumbnail} className='accommodation_thumbnail' alt='NA'></img>
+				<p>{vacay.name}</p>
+				<p>{vacay.location}</p>
+				<p>From Kes: {vacay.price}</p>
+				<p>Rating: {vacay.rating}</p>
+				<button>Book Now</button>
+			</div>
+		)
+	})
+
   	return (
     	<div className='hot_deals container'>
 			<h3>Top Vacations</h3>
-			<div className='card_container d-flex gap-5'>
-				<div className='safari_card mt-2'>
-					<img src='https://viutravel-cms-bucket.s3.eu-west-1.amazonaws.com/11582/conversions/WhatsApp-Image-2023-06-06-at-10.47.22-AM-thumbnail.webp' alt='NA'></img>
-					<p>Location</p>
-					<p>Rating</p>
-					<p>Price</p>
-					<button>Book Now</button>
-				</div>
-
-				<div className='safari_card mt-2'>
-					<img src='https://viutravel-cms-bucket.s3.eu-west-1.amazonaws.com/4025/conversions/Maasai-Mara-Thumbnail-2-thumbnail.webp' alt='NA'></img>
-					<p>Location</p>
-					<p>Rating</p>
-					<p>Price</p>
-					<button>Book Now</button>
-				</div>
-
-				<div className='safari_card mt-2'>
-					<img src='https://viutravel-cms-bucket.s3.eu-west-1.amazonaws.com/102/conversions/rsz-Ecotourism-Thumb-thumbnail.webp' alt='NA'></img>
-					<p>Location</p>
-					<p>Rating</p>
-					<p>Price</p>
-					<button>Book Now</button>
-				</div>
-
-				<div className='safari_card mt-2'>
-					<img src='https://viutravel-cms-bucket.s3.eu-west-1.amazonaws.com/122/conversions/rsz-Romantic-Thumb-%282%29-thumbnail.webp' alt='NA'></img>
-					<p>Location</p>
-					<p>Rating</p>
-					<p>Price</p>
-					<button>Book Now</button>
-				</div>
+			<div className='card_container d-flex flex-wrap justify-content-center  gap-5'>
+				{vacay_cards}
 			</div>
     	</div>
   	)
