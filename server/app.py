027a956fb9e934d69c553324c097a482d9fd843f
Gad-Ongoro <gadongoro1@gmail.com>
2024-01-31 18:54:11 +0300

    JWT

--- server/app.py
+++ server/app.py
@@ -1,11 +1,13 @@
-from config import db, app, bcrypt, render_template
-from config import api, Resource, request, make_response, jsonify, session, abort, jwt
+from config import db, app, bcrypt, render_template, jwt_required
+from config import api, Resource, request, make_response, jsonify, session, abort
 from models import Vacation, Accommodation, Customer, Customer_Profile, Paymentdetail, Booking
 from controllers.newsletter_mails_controller import newsletter_mail_bp
 from controllers.customers_controller import customers_bp
+from controllers.customer_login_controller import log_in_bp
 
 app.register_blueprint(newsletter_mail_bp)
 app.register_blueprint(customers_bp)
+app.register_blueprint(log_in_bp)
 
 """ HOME """
 @app.route('/', methods=['get'])
@@ -23,132 +25,6 @@ class CheckSession(Resource):
         except Exception as e:
             return {'token': None}, 404
 api.add_resource(CheckSession, '/check_session')
-
-
-""" LOGIN """
-class LogUserIn(Resource):
-    def post(self):
-        try:
-            data = request.get_json()
-            if not data:
-                return {
-                    "message": "Please provide user details",
-                    "data": None,
-                    "error": "Bad request"
-                }, 400
-
-            # validate input
-            email = data["email"]
-            user = Customer.query.filter_by(email == email).first()
-            if not user:
-                return {
-                    "message": "Please provide user details",
-                    "data": None,
-                    "error": "Username not found"
-                }, 404
-
-            password = data['password']
-            if bcrypt.check_password_hash(user.password, password):
-                try:
-                    # token should expire after 24 hrs
-                    user.token = jwt.encode(
-                        {"user_id": str(user.customer_id)},
-                        app.config["SECRET_KEY"],
-                        algorithm="HS256"
-                    )
-                    # set token to session
-                    session['token'] = user.token
-
-                    return make_response(jsonify({
-                        "message": "Successfully fetched auth token",
-                        "data": user.to_dict(),
-                        "token": str(user.token)
-                    }), 200)
-                except Exception as e:
-                    return {
-                        "error": "Something went wrong",
-                        "message": str(e)
-                    }, 500
-            return {
-                "message": "Error fetching auth token!.",
-                "data": None,
-                "error": "Invalid username or password. Try again.."
-            }, 404
-        except Exception as e:
-            return {
-                "message": "Something went wrong!",
-                "error": str(e),
-                "data": None
-            }, 500
-        
-        # data = request.get_json()
-        # customer_email = data.get('email')
-        # password = data.get("password")
-        
-        # customer = Customer.query.filter_by(email = customer_email).first().to_dict()
-        # response = make_response(jsonify(customer))
-        
-        # try:
-        #     if not data:
-        #         return({'message': 'Please provide the required inputs'}, 400)
-        #     customer = Customer.query.filter_by(email = customer_email).first()
-        #     response = make_response(jsonify(customer))
-            
-        #     if customer is None:
-        #         abort(404, detail=f'No Users with {customer_email} Found!')
-                
-        #     if not bcrypt.check_password_hash(customer.password, password):
-        #         abort(403, detail='Incorrect Password!')
-        #     else:
-        #         try:
-        #             # token should expire after 24 hrs
-        #             customer.token = jwt.encode(
-        #                 {'customer_id': str(customer.customer_id)},
-        #                 app.config["SECRET_KEY"],
-        #                 algorithm="HS256"
-        #             )
-                    
-        #             session['token'] = customer.token
-                    
-        #             return make_response(jsonify({
-        #                 "message": "Successfully fetched auth token",
-        #                 "data": customer.to_dict(),
-        #                 "token": str(customer.token)
-        #             }), 200)
-                    
-        #         except Exception as e:
-        #             return (
-        #                 {
-        #                 "error": "Something went wrong",
-        #                 "message": str(e)
-        #             }, 500
-        #             )
-            
-        # except Exception as e:
-        #     return(
-        #         {"message": "Something went wrong!", 
-        #             "error": str(e),
-        #             "data": None
-        #         }, 500)
-
-        
-        
-        
-
-        # token = create_access_token(identity=customer.customer_id)
-        # return({'jwt': token})
-        
-        # session['customer_id'] = customer.customer_id
-        
-        # return(jsonify({"Message" : f'Welcome {customer.user_name}'}))
-        
-        # customer_dict = customer.to_dict()
-        
-        # response = make_response(jsonify(customer_dict))
-        # response = make_response(jsonify(customer))
-        return response
-        
-api.add_resource(LogUserIn, '/login')
 
 
 """ CUSTOMER PROFILE """
@@ -187,6 +63,7 @@ api.add_resource(Customer_Bookings, '/customer_bookings/<int:id>')
 
 """ VACATIONS """
 class All_Vacations(Resource):
+    @jwt_required()
     def get(self):
         vacations = [vacay.to_dict() for vacay in Vacation.query.all()]
             
