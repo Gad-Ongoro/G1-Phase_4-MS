1ab8492f439b68020be16eee942991553be22c68
Gad-Ongoro <gadongoro1@gmail.com>
2024-03-15 13:13:02 +0300

    feat: Added python libraries and environment variable

--- server/config.py
+++ server/config.py
@@ -1,3 +1,6 @@
+import os
+from dotenv import load_dotenv
+load_dotenv()
 from flask import Flask, render_template, request, make_response, jsonify, session, abort
 from flask_bcrypt import Bcrypt
 from flask_cors import CORS
@@ -19,6 +22,7 @@ app = Flask(__name__)
 app = Flask(__name__)
 app.secret_key = b'\xb2_8\xcc\xfc\xec3n\xc5\x7f\x01-\xdal[\xc7'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///safaris.db'
+# app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URI')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config["JWT_SECRET_KEY"] = b'\xb2_8\xcc\xfc\xec3n\xc5\x7f\x01-\xdal[\xc7'
 app.config['JWT_ACCESS_TOKEN_EXPIRES'] = timedelta(hours=24)
